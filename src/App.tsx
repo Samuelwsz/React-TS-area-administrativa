3ff4885adc72042a6d0f9b72a1c2b19fbca962c1
Samuelwsz <Samuelxwsz@gmail.com>
2023-07-17 16:25:50 -0300

    att

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,27 @@ import Container from "./componentes/Container"
 import Titulo from "./componentes/Titulo"
 import Cabecalho from "./componentes/Cabecalho"
 import Rodape from "./componentes/Rodape"
+import Tabela from "./componentes/Tabela"
+import Grafico from "./componentes/Grafico"
+
+import useDadosProfissional from "./utils/useDadosProfissional"
+import useDadosConsulta from "./utils/useDadosConsulta"
 
 function App() {
+  const { dados: consultas, erro: consultasErro } = useDadosConsulta()
+  const { dados: profissionais, erro: profissionaisErro } =
+    useDadosProfissional()
+
+  if (consultasErro || profissionaisErro) {
+    console.log("Erro")
+  }
   return (
     <>
       <Cabecalho />
       <Container>
         <Titulo>Área Administrativa</Titulo>
+        <Tabela consultas={consultas} />
+        <Grafico consultas={consultas} profissionais={profissionais} />
       </Container>
       <Rodape />
     </>
---
+++ src/componentes/Grafico/useDadosGrafico.tsx
@@ -0,0 +1,27 @@
+import IConsulta from "../../types/IConsulta"
+import IProfissional from "../../types/IProfissional"
+import { IDados } from "../../utils/IDados"
+
+interface useDadosGraficosProps {
+  profissionais: IProfissional[] | null
+  consultas: IConsulta[] | null
+}
+
+const useDadosGraficos = ({
+  profissionais,
+  consultas,
+}: useDadosGraficosProps) => {
+  let dados: Array<IDados> = []
+
+  if (profissionais && consultas) {
+    dados = profissionais.map((profissional) => ({
+      nome: profissional.nome,
+      consultas: consultas.filter((consulta) =>
+        consulta.profissional.some((prof) => prof.nome === profissional.nome)
+      ).length,
+    }))
+  }
+  return dados
+}
+
+export default useDadosGraficos
